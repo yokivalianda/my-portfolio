238561036f391221ff4821d981c51cefc3618b4b
Yoki Valianda <yokivalianda14@gmail.com>
2026-04-06 10:47:02 +0700

    update major

---
+++ js/firebase-config.js
@@ -0,0 +1,8 @@
+const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyBSNV4hCk83Th8Ni4mWJ4lRx76lWxzoJ8I",
+  authDomain: "portofolio-yoki.firebaseapp.com",
+  projectId: "portofolio-yoki",
+  storageBucket: "portofolio-yoki.firebasestorage.app",
+  messagingSenderId: "637400395715",
+  appId: "1:637400395715:web:79b025712e8a85f3869776"
+};
